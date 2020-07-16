b94246ba1e6dd0470eb3eccd152000bba2ffce89
User Name <artur.it2914@gmail.com>
2020-07-16 17:14:32 +0300

    Add spread operator explanation

--- map.js
+++ map.js
@@ -11,6 +11,30 @@ vocabulary.set( 'update', 'обновить' );
 vocabulary.set( 'delete', 'удалить' );
 
 vocabulary.delete( 'f' );
+const keysIter = vocabulary.keys();
+const vocabularyKeys = [...keysIter];
+const vocabularyValues = [...vocabulary.values()];
+
+const arr1 = [1, 2, 2, , 7, 52, 1,];
+const arr2 = [1, 2, 2, , 7, 52, 1,];
+const arr1Iter = arr1[Symbol.iterator]();
+const result = [...arr1Iter, ...arr2];
+
+
+function getMapKeys(map) {
+  const keyArray = [];
+  const keysIterator = map.keys();
+  let nextObj = keysIterator.next();
+  while (!nextObj.done) {
+
+    keyArray.push( nextObj.value );
+    nextObj = keysIterator.next();
+  }
+  return keyArray;
+}
+
+console.table( vocabularyKeys );
+console.table( getMapKeys( vocabulary ) );
 
 const translate = (str, separator = ' ') => str
   .split( separator )
