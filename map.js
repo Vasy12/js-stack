71f73566eee1151db7242509931b704e0030e2e0
User Name <artur.it2914@gmail.com>
2020-07-16 16:33:03 +0300

    Add vocabulary map example

--- map.js
+++ map.js
@@ -10,9 +10,28 @@ vocabulary.set( 'read', 'прочитать' );
 vocabulary.set( 'update', 'обновить' );
 vocabulary.set( 'delete', 'удалить' );
 
+vocabulary.delete( 'f' );
 
+const translate = (str, separator = ' ') => str
+  .split( separator )
+  .map( w => vocabulary.has( w )
+             ? vocabulary.get( w )
+             : w )
+  .join( separator );
 
-const translate = str => str.split( ' ' ).map( w => vocabulary.get( w ) ).join( ' ' );
+const str = 'cat dog read squirrel monkey';
+console.table( fTranslate( str ) );
 
-const str = 'cat dog read squirrel';
-alert( translate( str ) );
+function fTranslate(str, separator = ' ') {
+
+  const englishWords = str.split( separator ); // Array
+
+  const russianWords = englishWords.map( function (word) {
+    if (vocabulary.has( word )) {
+      return vocabulary.get( word );
+    }
+    return word;
+  } ); // Array
+
+  return russianWords.join( separator ); // string
+}
